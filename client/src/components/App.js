ab2bf83d07337495a2ebfedcd7425932c6926ef6
Jason Crout <croutjason@gmail.com>
2022-02-18 17:51:47 +1100

    hi my name is jason

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hi my name is jason.</h1>
+      <p>this may take a while</p>
+    </div>
+  );
+}
+
+export default App;
