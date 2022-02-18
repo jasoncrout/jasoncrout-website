90854fc1252e695b34c808194827125be7985247
Jason Crout <croutjason@gmail.com>
2022-02-18 16:15:35 +1100

    boilerplate

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config()
+const express = require("express");
+const path = require("path")
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(process.env.PORT || 8000, function() {
+    console.log("Server started successfully!");
+});
